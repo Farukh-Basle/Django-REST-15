51bc85c6efc41750ccfe8de368178ce6dadce6df
Md Farukh Basle <66154796+Farukh-Basle@users.noreply.github.com>
2021-09-14 12:22:13 +0530

    Update settings.py

--- JWTAuthentication_Project/JWTAuthentication_Project/settings.py
+++ JWTAuthentication_Project/JWTAuthentication_Project/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'JWTAuthentication_Project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'nth_rest9amdb_jwttokendb',
+        'NAME': 'jwttokendb',
         'USER' : 'root',
         'PASSWORD' : 'root',
     }
